10f0bd1c5449437f46cf07870b4217b22694f219
Moradell <TheMoradell@gmail.com>
2020-12-29 22:29:04 +1000

    Update map.js

--- js/map.js
+++ js/map.js
@@ -17,7 +17,7 @@ const init = () => {
  const myCollection = new ymaps.GeoObjectCollection({}, {
    draggable: false,
    iconLayout: 'default#image',
-   iconImageHref: "../img/marker.png",
+   iconImageHref: "chocco/img/marker.svg",
    iconImageSize: [46, 57],
    iconImageOffset: [-25, -52]
  });
